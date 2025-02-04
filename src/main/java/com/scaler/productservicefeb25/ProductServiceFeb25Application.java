5a5492483dc4c18a04b79f4321a8246fe0d9af15
deepakkasera <deepak.kasera143.dk@gmail.com>
2025-02-04 09:37:03 +0530

    Product Service implementation

--- src/main/java/com/scaler/productservicefeb25/ProductServiceFeb25Application.java
+++ src/main/java/com/scaler/productservicefeb25/ProductServiceFeb25Application.java
@@ -1,13 +1,12 @@
 package com.scaler.productservicefeb25;
 
+import com.scaler.productservicefeb25.models.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductServiceFeb25Application {
-
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceFeb25Application.class, args);
     }
-
 }
---
+++ src/main/java/com/scaler/productservicefeb25/controllers/ProductController.java
@@ -0,0 +1,46 @@
+package com.scaler.productservicefeb25.controllers;
+
+import com.scaler.productservicefeb25.models.Product;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    // http://localhost:8080/products/1 => GET
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable("id") Long id) {
+        return null;
+    }
+
+    // http://localhost:8080/products
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping()
+    public Product createProduct(@RequestBody Product product) {
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public Product updateProduct(@PathVariable("id") Long productId,
+                                 @RequestBody Product product) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public Product replaceProduct(@PathVariable("id") Long productId,
+                                  @RequestBody Product product) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMapping(@PathVariable("id") Long id) {
+
+    }
+}
